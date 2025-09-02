fa90f266679ed046728e7b545729307666bdbd4d
jelambrar96 <jelambrar@gmail.com>
2025-09-01 22:04:30 -0500

    cors

--- backend/app/core/cors.py
+++ backend/app/core/cors.py
@@ -1,20 +1,20 @@
 from fastapi.middleware.cors import CORSMiddleware
 
-
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://frontend",
-    "http://frontend:8080",
-    "*"
-]
+from app.core.config import CORS_HEADERS, CORS_METHODS, CORS_ORIGINS
 
 
-def add_middleware(app):
+origins = [ "http://localhost", "http://localhost:8080", "http://frontend", "http://frontend:8080"] \
+        if CORS_ORIGINS is None else CORS_ORIGINS.split(",")
+
+methods = ["GET", "POST", "PUT", "DELETE"] if CORS_METHODS else CORS_METHODS.split(",")
+headers = ["Authorization", "Content-Type"] if CORS_HEADERS else CORS_HEADERS.split(",")
+
+
+def add_cors_middleware(app):
     app.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
         allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
+        allow_methods=methods,
+        allow_headers=headers,
     )
--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+from app.core.cors import add_cors_middleware
 from app.db.database import create_db_and_tables, fill_task_priority_table, create_super_user
 from app.routes.root import root_routers
 from app.routes.tasks import tasks_routers
@@ -20,6 +21,7 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+add_cors_middleware(app=app)
 
 app.include_router(root_routers)
 app.include_router(tasks_routers)
